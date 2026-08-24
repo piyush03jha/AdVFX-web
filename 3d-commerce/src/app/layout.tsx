62ed66ebe9c527ed4d545228fa128349a0435e18
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-24 17:59:17 +0530

    Mount global cart provider

--- 3d-commerce/src/app/layout.tsx
+++ 3d-commerce/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Footer } from "@/components/layout/Footer";
+import { CartProvider } from "@/context/CartContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <CartProvider>
+          {children}
+        </CartProvider>
 
         <Footer />
       </body>
