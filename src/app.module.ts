787656e6ac2be09b813f098d9673047383e16fb6
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 17:35:18 +0530

    feat: register shipment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { ProcessingJobsModule } from "./processing-jobs/processing-jobs.module";
 import { CategoriesModule } from "./categories/categories.module";
 import { CartModule } from "./cart/cart.module";
 import { OrdersModule } from "./orders/orders.module";
+import { ShipmentsModule } from "./shipments/shipments.module";
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { OrdersModule } from "./orders/orders.module";
     ProcessingJobsModule,
     CartModule,
     OrdersModule,
+    ShipmentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
