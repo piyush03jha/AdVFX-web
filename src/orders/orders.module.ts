61e479087df526989beaea4305b73f158d3a4e66
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 19:31:13 +0530

    feat: wire pricing into orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { PricingModule } from '../pricing/pricing.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [PrismaModule, NotificationsModule],
+  imports: [PrismaModule, NotificationsModule, PricingModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
