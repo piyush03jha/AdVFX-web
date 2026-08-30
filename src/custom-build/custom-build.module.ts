09985790a1988bf0b64718429fa9e1e79ad78c86
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 18:52:40 +0530

    Integrate cart service with custom builds

--- src/custom-build/custom-build.module.ts
+++ src/custom-build/custom-build.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
+import { CartModule } from '../cart/cart.module';
 import { CustomBuildController } from './custom-build.controller';
 import { CustomBuildFilesController } from './custom-build.files.controller';
 import { CustomBuildService } from './custom-build.service';
 
 @Module({
-  imports: [PrismaModule, StorageModule],
+  imports: [PrismaModule, StorageModule, CartModule],
   controllers: [CustomBuildController, CustomBuildFilesController],
   providers: [CustomBuildService],
   exports: [CustomBuildService],
