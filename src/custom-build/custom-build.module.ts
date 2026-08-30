126015f4fec69ebd2d484f440ff7082143b530f5
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 18:39:13 +0530

    Wire custom request file uploads

--- src/custom-build/custom-build.module.ts
+++ src/custom-build/custom-build.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 import { CustomBuildController } from './custom-build.controller';
+import { CustomBuildFilesController } from './custom-build.files.controller';
 import { CustomBuildService } from './custom-build.service';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [CustomBuildController],
+  imports: [PrismaModule, StorageModule],
+  controllers: [CustomBuildController, CustomBuildFilesController],
   providers: [CustomBuildService],
   exports: [CustomBuildService],
 })
