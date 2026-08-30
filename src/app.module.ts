c22562417a34fbe1630042d0e6d6ea855285c341
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 15:17:11 +0530

    Register catalog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { ProductsModule } from "./products/products.module";
 import { ProductFilesModule } from "./product-files/product-files.module";
 import { ProcessingJobsModule } from "./processing-jobs/processing-jobs.module";
+import { CategoriesModule } from "./categories/categories.module";
 
 @Module({
   imports: [
     PrismaModule,
+    CategoriesModule,
     ProductsModule,
     ProductFilesModule,
     ProcessingJobsModule,
