a0ae538a65ddfa9438eff218d33036e82d171ed6
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 19:16:51 +0530

    Wire shipping service into checkout

--- src/checkout/checkout.module.ts
+++ src/checkout/checkout.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ShippingModule } from '../shipping/shipping.module';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ShippingModule],
   controllers: [CheckoutController],
   providers: [CheckoutService],
   exports: [CheckoutService],
