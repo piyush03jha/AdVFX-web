910b26acc3a582f848de01f804385159ecd533e4
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 19:41:38 +0530

    feat: register return workflow

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,11 +4,12 @@ import { NotificationsModule } from '../notifications/notifications.module';
 import { PricingModule } from '../pricing/pricing.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ReturnsService } from './returns.service';
 
 @Module({
   imports: [PrismaModule, NotificationsModule, PricingModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
-  exports: [OrdersService],
+  providers: [OrdersService, ReturnsService],
+  exports: [OrdersService, ReturnsService],
 })
 export class OrdersModule {}
