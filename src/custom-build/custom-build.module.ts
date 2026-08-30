1235cbddee09c129aca9b5932e2435a1586bfa05
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-08-30 19:04:13 +0530

    Wire notifications into custom build

--- src/custom-build/custom-build.module.ts
+++ src/custom-build/custom-build.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
 import { CartModule } from '../cart/cart.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { CustomBuildController } from './custom-build.controller';
 import { CustomBuildFilesController } from './custom-build.files.controller';
 import { CustomBuildService } from './custom-build.service';
 
 @Module({
-  imports: [PrismaModule, StorageModule, CartModule],
+  imports: [PrismaModule, StorageModule, CartModule, NotificationsModule],
   controllers: [CustomBuildController, CustomBuildFilesController],
   providers: [CustomBuildService],
   exports: [CustomBuildService],
